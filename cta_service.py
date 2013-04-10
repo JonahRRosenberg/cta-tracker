008113abc2d56b7f48a11f512fbd219d9025f0ef
Jonah Rosenberg <JonahRRosenberg@gmail.com>
2013-04-09 22:30:05 -0500

    changed port to look for env variable

--- cta_service.py
+++ cta_service.py
@@ -1,6 +1,7 @@
 from bottle import route, run, SimpleTemplate, template
 from source import CTASource
 from client import CTAClient
+import os
 import logger
 
 logger.initialize()
@@ -16,19 +17,19 @@ def run_app():
       stations=client.stations,
       trains=client.trains,
       current_time=current_time)
-
-def shutdown():
-  server.socket.close()
         
 def main():
   try:
-    run(host='0.0.0.0', port=8000, debug=True)
+    run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)),
+        debug=True)
+    #run(host='0.0.0.0', port=8000,
+    #    debug=True)
   except KeyboardInterrupt:
     logger.info('^C Received. Shutting Down Server.')
-    shutdown()
+  except Exception as ex:
+    logger.error('Exception occurred. ex: ' + str(ex))
   except:
     logger.error('Unknown Exception occurred')
-    shutdown()
     
 if __name__ == '__main__':
   main()
