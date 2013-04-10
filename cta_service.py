7244ebb205b79b500c7b3c098d96a41f0f81ae7f
Jonah Rosenberg <JonahRRosenberg@gmail.com>
2013-04-09 22:51:35 -0500

    removed start with main

--- cta_service.py
+++ cta_service.py
@@ -4,11 +4,6 @@ from client import CTAClient
 import os
 import logger
 
-logger.initialize()
-
-cta_source = CTASource()
-client = CTAClient(cta_source)
-
 @route('/')
 def run_app():
   now_time = client.get_current_time()
@@ -17,19 +12,18 @@ def run_app():
       stations=client.stations,
       trains=client.trains,
       current_time=current_time)
-        
-def main():
-  try:
-    run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)),
-        debug=True)
-    #run(host='0.0.0.0', port=8000,
-    #    debug=True)
-  except KeyboardInterrupt:
-    logger.info('^C Received. Shutting Down Server.')
-  except Exception as ex:
-    logger.error('Exception occurred. ex: ' + str(ex))
-  except:
-    logger.error('Unknown Exception occurred')
-    
-if __name__ == '__main__':
-  main()
+      
+logger.initialize()
+
+cta_source = CTASource()
+client = CTAClient(cta_source)
+      
+try:
+  run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)),
+      debug=True)
+except KeyboardInterrupt:
+  logger.info('^C Received. Shutting Down Server.')
+except Exception as ex:
+  logger.error('Exception occurred. ex: ' + str(ex))
+except:
+  logger.error('Unknown Exception occurred')
